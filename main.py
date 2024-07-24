b85140938204f66d2ceeeee125f88c3bc02fe3ec
invictus <invictus@gmail.com>
2024-07-23 20:55:29 -0400

    Added python typer

--- main.py
+++ main.py
@@ -7,33 +7,39 @@ from psutil._common import bytes2human
 import os
 import datetime
 import speedtest 
+import typer
+
+app = typer.Typer()
 
 
-def Get_Weather():
+
+@app.command()
+def Weather():
     ip = Get_IP()
     example_url ="http://api.weatherapi.com/v1/current.json?key="+Api_Key+"&q=" + ip
     Ans = r.get(example_url)
     Temperature = Ans.json()['current']['temp_c']
     Condition = Ans.json()['current']['condition']['text']
-    return (Temperature,Condition)
+    print (Temperature,Condition)
 
 def Get_IP():
     Address = r.get('http://checkip.dyndns.com/')
     return re.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(Address.text).group(1)
 
-
-def Get_Mem():
+@app.command()
+def Memory():
     mem = psutil.virtual_memory()
     Percent = mem[2]
-    return (f"Memoru Usage : {Percent} %")
+    print (f"Memoru Usage : {Percent} %")
 
-def Get_Disk():
+@app.command()
+def Disk():
     Disk = psutil.disk_usage('/') 
     Usage = int(Disk.percent)
-    return (f"Disk Usage : {Usage} %") 
+    print (f"Disk Usage : {Usage} %") 
 
-
-def Get_Network(Up = False):
+@app.command()
+def Network_Speed(Up : bool = False):
     servers = []
     threads = 1
     s = speedtest.Speedtest()
@@ -43,12 +49,14 @@ def Get_Network(Up = False):
     download_mbs = round(Download / (10**6), 2)
     upload_mbs = round(Upload / (10**6), 2)
     if(Up == True):
-        return (f'Dow : {download_mbs}Mbits/ Up : {upload_mbs}Mbits')
+        print (f'Dow : {download_mbs}Mbits/ Up : {upload_mbs}Mbits')
     else: 
-        return (f'Dow : {download_mbs}Mbits')
+        print (f'Dow : {download_mbs}Mbits')
 
-
-def Get_Time_Date():
+@app.command()
+def Time_Date():
     Date_Time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M') 
-    return (Date_Time)
-#print(Get_Disk())
+    print (Date_Time)
+
+if __name__ == "__main__":
+    app()
